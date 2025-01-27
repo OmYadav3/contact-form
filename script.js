3a5ed3e21462f77f33884d565b3fe01c0fde2c2e
OmYadav <omydv3@gmail.com>
2025-01-27 19:02:19 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const nameInput = document.getElementById("name_input");
+const lastNameInput = document.getElementById("lastname_input");
+const formSubmit = document.getElementById("submit")
+
+
+// function submit_Form(){
+    
+// }
+
+
+// formSubmit.addEventListener("click", submit_Form());
